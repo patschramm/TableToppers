4c4ec69506c8641a3b67e436a48d8a2c0d4142fd
patschramm <>
2021-06-02 11:21:17 -0700

    added users and sessions controllers, worked on routes

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+class Api::UsersController < ApplicationController
+  before_action :require_login, only: [:show, :index]
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+      @user = User.find(params[:id])
+      render :show
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
